37f088ace87e5116703e46fa4884015516a20a9d
Amdronikus <tiagoafroque@gmail.com>
2021-03-10 16:12:09 +0000

    add ResourceTable class

---
+++ src/main/java/pt/andronikus/database/tables/ResourceTable.java
@@ -0,0 +1,47 @@
+package pt.andronikus.database.tables;
+
+public class ResourceTable {
+    public final static String CDM_SERVICE_INSTANCE = "CDM_RESOURCES";
+
+    // columns
+    public final static String OPERATOR_ID = "OPERATOR_ID";
+    public final static String MSISDN = "MSISDN";
+    public final static String PF = "PF";
+    public final static String AGREEMENT_ID = "AGREEMENT_ID";
+    public final static String SERVICE_INSTANCE_ID = "SERVICE_INSTANCE_ID";
+    public final static String RESOURCE_HOME_NETWORK = "RESOURCE_HOME_NETWORK";
+    public final static String CATALOG_SPEC = "CATALOG_SPEC";
+    public final static String OFFER_SPEC = "OFFER_SPEC";
+    public final static String SECONDARY_MSISDN = "SECONDARY_MSISDN";
+    public final static String ICCID = "ICCID";
+    public final static String SERVICE_RESOURCE_STATUS = "SERVICE_RESOURCE_STATUS";
+    public final static String TESTING_LIFE_CYCLE_ENABLED = "TESTING_LIFE_CYCLE_ENABLED";
+    public final static String COMM_STATUS = "COMM_STATUS";
+    public final static String REACTIVATE_COMM_STATUS = "REACTIVATE_COMM_STATUS";
+    public final static String SMS_SERVICE = "SMS_SERVICE";
+    public final static String VOICE_SERVICE = "VOICE_SERVICE";
+    public final static String DATA_CS_SERVICE = "DATA_CS_SERVICE";
+    public final static String DATA_PS_SERVICE = "DATA_PS_SERVICE";
+    public final static String SMS_TARIFF_PLAN = "SMS_TARIFF_PLAN";
+    public final static String VOICE_TARIFF_PLAN = "VOICE_TARIFF_PLAN";
+    public final static String DATA_CS_TARIFF_PLAN = "DATA_CS_TARIFF_PLAN";
+    public final static String DATA_PS_TARIFF_PLAN = "DATA_PS_TARIFF_PLAN";
+    public final static String ADMIN_RESOURCE_STATUS = "ADMIN_RESOURCE_STATUS";
+    public final static String CREATION_DATE = "CREATION_DATE";
+    public final static String FIRST_ACTIVATION_DATE = "FIRST_ACTIVATION_DATE";
+    public final static String DESIGNATION = "DESIGNATION";
+    public final static String ROAMING_STATUS = "ROAMING_STATUS";
+    public final static String ENDPOINT = "ENDPOINT";
+    public final static String ENDPOINT_GROUPS = "ENDPOINT_GROUPS";
+    public final static String APN_ID = "APN_ID";
+    public final static String APN_INDEX = "APN_INDEX";
+    public final static String APN_TYPE = "APN_TYPE";
+    public final static String APN_QOS = "APN_QOS";
+    public final static String LOYALTY_PERIOD_REMAINING = "LOYALTY_PERIOD_REMAINING";
+    public final static String LOYALTY_LAST_UPDATE = "LOYALTY_LAST_UPDATE";
+    public final static String CORRELATION_ID = "CORRELATION_ID";
+    public final static String ORDER_CORRELATION_ID = "ORDER_CORRELATION_ID";
+    public final static String MIG_STATUS = "MIG_STATUS";
+
+
+}
