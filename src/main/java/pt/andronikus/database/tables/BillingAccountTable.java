c1e6564f2386efaf9f57662b6513d210175e1e21
Amdronikus <tiagoafroque@gmail.com>
2021-03-10 14:52:40 +0000

    add BillingAccountTable class

---
+++ src/main/java/pt/andronikus/database/tables/BillingAccountTable.java
@@ -0,0 +1,18 @@
+package pt.andronikus.database.tables;
+
+public class BillingAccountTable {
+    public final static String CDM_BILLING_ACCOUNT = "CDM_BILLING_ACCOUNT";
+
+    // columns
+    public final static String OPERATOR_ID = "OPERATOR_ID";
+    public final static String ACCOUNT_ID = "ACCOUNT_ID";
+    public final static String PF = "PF";
+    public final static String ACCOUNT_NAME = "ACCOUNT_NAME";
+    public final static String CUSTOMER_ID = "CUSTOMER_ID";
+    public final static String BILLING_CYCLE_DAY = "BILLING_CYCLE_DAY";
+    public final static String CORRELATION_ID = "CORRELATION_ID";
+    public final static String ORDER_CORRELATION_ID = "ORDER_CORRELATION_ID";
+    public final static String MIG_STATUS = "MIG_STATUS";
+
+    // queries
+}
